ffe939e779091f62319095f2b9715ae0effaefdb
Jesus Rodriguez <Foxandxss@gmail.com>
2013-06-20 12:50:30 +0200

    Move a method into protected section.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
 
   def index
   	render file: Rails.root.join("public/index.html"), layout: false
-  end
+  end  
+
+  protected
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
-
-  protected
 
   def verified_request?
     super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
